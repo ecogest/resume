d23938bdfcd9850fe4011aedfc727456ae23dd3d
mjacq <>
2022-09-22 18:02:37 +0200

    sidebar: try

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SubsectionComponent } from './components/subsection/subsection.component';
 import { SectionComponent } from './components/section/section.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [AppComponent, SubsectionComponent, SectionComponent],
+  declarations: [
+    AppComponent,
+    SubsectionComponent,
+    SectionComponent,
+    HeaderComponent,
+    SidebarComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
