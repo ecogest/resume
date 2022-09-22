2c4b959152a422926e972bb0f403f4b3e1c12af5
mjacq <>
2022-09-22 23:06:43 +0200

    header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  email = 'matthieu.jacq@live.fr';
+  phone = '06 15 85 41 10';
+  linkedin = {
+    name: 'matthieu-jacq',
+    address: 'https://www.linkedin.com/in/matthieu-jacq',
+  };
+  github = { name: 'ecogest', address: 'https://github.com/ecogest' };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
